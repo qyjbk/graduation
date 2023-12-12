bfa2e4bd321e4829197282a317de1e19661404df
shenkw <2644808362@qq.com>
2023-12-12 20:12:44 +0800

    标题:将iconfont的svg图标引入，起步，下一步导入中英文字体
    项目:毕业设计
    
    描述:起步项目
    
    问题:
    
    原因:
    
    解决方案:
    
    作者:shenkw
    
    日期:2023-12-12

---
+++ src/components/Heading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="heading__logo">
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-diannao"></use>
+    </svg>
+    <p>DIYPlayer</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.heading__logo {
+  margin: 3rem 1.5rem;
+  :not(:first-child){
+    margin-left: .5rem;
+  }
+}
+</style>
---
+++ src/components/temp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="heading__logo">
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-dengpao"></use>
+    </svg>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
