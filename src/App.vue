a5d2930ff570f4075fbdf64a19c8597fc645851c
shenkw <2644808362@qq.com>
2023-12-13 20:02:03 +0800

    标题:提交完成字体
    项目:
    
    描述:
    
    问题:
    
    原因:
    
    解决方案:
    
    作者:shenkw
    
    日期:2023-12-13

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
 import Heading from './components/Heading.vue'
-
+import MainContent from './components/MainContent.vue'
 </script>
 
 <template>
   <section class="heading">
-    <Heading class="heading__box"></Heading>
+    <Heading></Heading>
+  </section>
+  <section class="main">
+    <h1 class="main__title">推荐电脑搭配</h1>
+    <MainContent></MainContent>
   </section>
 </template>
 
 <style lang="scss" scoped>
-.heading__box {
+.heading {
   display: flex;
+}
+.main {
+  &__title {
+    margin-top: 0;
+    font-weight: 200;
+    font-size: 2.2rem;
+    margin-left: 1.5rem;
+    letter-spacing: .2rem;
+  }
 }
 
 </style>
--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="heading__logo">
+  <div class="heading__left">
     <svg class="icon" aria-hidden="true">
       <use xlink:href="#icon-diannao"></use>
     </svg>
-    <p>DIYPlayer</p>
+    <p class="english">DIYPlayer</p>
+  </div>
+  <div class="heading__right">
+    <user></user>
+    <a href="#">联系我</a>
+    <a href="#">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-diannao"></use>
+      </svg>
+    </a>
+    <a href="#">关于网页</a>
   </div>
 </template>
 
@@ -11,10 +21,15 @@
 </script>
 
 <style lang="scss" scoped>
-.heading__logo {
-  margin: 3rem 1.5rem;
+.heading__left {
+  display: flex;
+  margin: 3rem 0rem 2rem 1.5rem;
   :not(:first-child){
     margin-left: .5rem;
   }
+}
+.heading__right {
+  justify-self: flex-end;
+  justify-content: flex-end;
 }
 </style>
