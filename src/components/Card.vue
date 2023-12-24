66c7c8a09f65c6c31d28eb8fe63fedf6487e2faa
shenkw <2644808362@qq.com>
2023-12-24 14:04:28 +0800

    标题:完成card组件的逐个激活，
    项目:毕设
    
    未来: 增加右下角的github图标和email，学习gsap，进行切换动画效果
    
    问题:
    
    原因:
    
    解决方案:
    
    作者:shenkw
    
    日期:2023-12-24

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,45 @@
 <template>
-    <div class="card">
-        <div class="card__name"><h2>海景房</h2></div>
+    <div class="card" ref="active">
+        <div class="card__name">
+            <h2>{{ card.name }}</h2>
+        </div>
         <div class="card__picbox">
             <img class="card__picbox__pic" src="../assets/imgs/cp1.png" alt="电脑照片">
         </div>
         <div class="card__bottom">
             <div class="card__bottom__price">
                 <div class="card__bottom__price__des">Price</div>
-                <div class="card__bottom__price__num">￥5999</div>
+                <div class="card__bottom__price__num">￥{{ card.price }}</div>
             </div>
             <a href="#" class="card__bottom__link">+</a>
         </div>
-        
+
     </div>
 </template>
 
 <script setup>
+import { defineProps, defineExpose, ref, onMounted } from 'vue';
+const active = ref({})
+const props = defineProps(['card', 'changeActiveItem'])
+const card = props.card
+// 激活card的选中状态
+const activeFunc = () => {
+    active.value.classList.add('active')
+    return card.id
+}
+// 取消激活card的选中状态
+const unActiveFunc = () => {
+    active.value.classList.remove('active')
+    return card.id
+}
+onMounted(() => {
+    active.value.addEventListener('mouseover', function () {
+        props.changeActiveItem(card.id)
+        active.value.classList.add('active')
+    });
+})
 
+defineExpose({ activeFunc, unActiveFunc })
 </script>
 
 <style lang="scss" scoped>
@@ -25,47 +48,55 @@
     height: 80%;
     color: var(--color-text-white);
 
-
     transition: all 0.8s;
+
     // transform-origin: center;
+    // &:hover {
+    //     height: 100%;
+    //     border-radius: 3rem 3rem 0 0;
+    //     background-color: rgb(71, 79, 113);
+    // }
+
+    // &:hover &__picbox {
+    //     width: 110%;
+    //     height: 55%;
+    // }
+
+    // &:hover &__bottom__price__des {
+    //     color: rgb(129, 128, 159);
+    // }
+
+    // &:hover &__bottom__link {
+    //     background-color: rgb(236, 97, 89);
+    // }
     &:hover {
-        height: 100%;
-        border-radius: 3rem 3rem 0 0 ;
-        background-color: rgb(71,79,113);
-    }
-
-    &:hover &__picbox {
-        width: 130%;
-        height: 65%;
-    }
-
-    &:hover &__bottom__price__des {
-        color: rgb(129,128,159);
-    }
-
-    &:hover &__bottom__link {
-        background-color: rgb(236,97,89);
+        cursor: pointer;
     }
 
     &__name {
-        margin-top: 8rem;
+        margin-top: 6rem;
         margin-left: 7rem;
-        margin-bottom: 0;
+        margin-bottom: 2rem;
+
         h2 {
             font-size: 2.2rem;
             font-weight: 100;
         }
     }
+
     &__picbox {
         transition: all 0.8s;
         margin: 0;
-        width: 100%;
-        height: 60%;
+        width: 85%;
+        height: 50%;
+        margin-left: 1rem;
+
         &__pic {
             width: 100%;
             height: 100%;
         }
     }
+
     &__bottom {
         margin-top: 2rem;
         margin-left: 7rem;
@@ -73,10 +104,12 @@
 
         &__price {
             margin-right: 4rem;
+
             &__des {
                 transform: all 1s;
                 font-size: 1.4rem;
             }
+
             &__num {
                 font-size: 2rem;
             }
@@ -84,6 +117,7 @@
 
         &__link {
             transition: all 0.8s;
+
             &:link,
             &:visited {
                 display: block;
@@ -98,6 +132,7 @@
                 color: var(--color-text-white);
                 text-decoration: none;
             }
+
             &:hover,
             &:active {
                 transform: scale(.9);
@@ -106,4 +141,21 @@
     }
 }
 
-</style>
+.active {
+    height: 100%;
+    border-radius: 3rem 3rem 0 0;
+    background-color: rgb(71, 79, 113);
+
+    .card__picbox {
+        width: 110%;
+        height: 55%;
+    }
+
+    .card__bottom__price__des {
+        color: rgb(129, 128, 159);
+    }
+
+    .card__bottom__link {
+        background-color: rgb(236, 97, 89);
+    }
+}</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,11 +1,65 @@
 <template>
-    <div class="mainContent__flexBox">
-        <Card></Card>
+    <div class="mainContent__flexBox" >
+        <Card :card = "card[0]" :changeActiveItem="changeActiveItem" ref="card1"></Card>
+        <Card :card = "card[1]" :changeActiveItem="changeActiveItem" ref="card2"></Card>
+        <Card :card = "card[2]" :changeActiveItem="changeActiveItem" ref="card3"></Card>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted, watch } from 'vue'
 import Card from './Card.vue'
+
+// 对每个card标签进行标记
+const card1 = ref('card1')
+const card2 = ref('card2')
+const card3 = ref('card3')
+
+// 用cardObj来存储card标签，利于后续操作
+const cardObj = {
+    card1,
+    card2,
+    card3
+}
+
+// 对当前激活的card标签进行标记
+const activeItem = ref(1)
+// 作为props传递给子组件，用于修改父组件记录的当前激活的card标签
+function changeActiveItem(id) {
+    activeItem.value = id
+}
+
+// 获取的card信息
+const card = [
+    {
+        id: 1,
+        name: '简朴',
+        imgSrc: '../assets/imgs/cp1.png',
+        price: '5999',
+    },
+    {
+        id: 2,
+        name: '海景房',
+        imgSrc: '../assets/imgs/cp1.png',
+        price: '15999',
+    },
+    {
+        id: 3,
+        name: '机械',
+        imgSrc: '../assets/imgs/cp1.png',
+        price: '8999',
+    },
+]
+
+// 监听事件, 使当前激活的card标签失去激活状态
+watch(() => activeItem.value, (newVal, oldVal) => {
+    cardObj[`card${oldVal}`].value.unActiveFunc()
+})
+
+onMounted(() => {
+    // 初始化第一个card标签，让其处于激活状态
+    card1.value.activeFunc()
+})
 </script>
 
 <style lang="scss" scoped>
@@ -15,5 +69,6 @@ import Card from './Card.vue'
     display: flex;
     justify-content: center;
     align-items: end;
+    gap: 5rem;
 }
 </style>
